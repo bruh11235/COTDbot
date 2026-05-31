9a17058dc20c56b4a1b9ed697b5c7cf8367f0350
dartvolley <84183321+bruh11235@users.noreply.github.com>
2026-05-30 22:16:48 -0400

    bot skeleton

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+from commands import setup_commands
+
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    await bot.tree.sync()
+    print("Slash commands synced")
+
+
+setup_commands(bot)
+
+bot.run(TOKEN)
